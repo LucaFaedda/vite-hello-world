3c564290c6f02d134dd70ce4c0adf85b627dc4b5
LucaFaedda <lucakarl91@gmail.com>
2023-01-10 23:06:48 +0100

    aggiunto appTitle

---
+++ hello_vite_project/src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script >
+export default{
+    data(){
+        return{
+            hello: 'ciao'
+        }
+    }
+}
+
+</script>
+
+<template>
+<h1>{{ hello }}</h1>
+</template>
+
+<style >
+</style>
